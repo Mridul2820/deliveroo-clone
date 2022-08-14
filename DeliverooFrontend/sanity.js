b141920d2859d0fd9d6926dd2f9558700f294c48
Mridul2820 <immridulpanda2820@gmail.com>
2022-08-14 13:22:26 +0530

    sanity build and connect to project

---
+++ DeliverooFrontend/sanity.js
@@ -0,0 +1,12 @@
+import SanityClient from '@sanity/client';
+import ImageUrlBuilder from '@sanity/image-url';
+
+const client = SanityClient({
+  projectId: 'skj4nexl',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2021-10-21',
+});
+
+const builder = ImageUrlBuilder(client);
+export const urlFor = source => builder.image(source);
